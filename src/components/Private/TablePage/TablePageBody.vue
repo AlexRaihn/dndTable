12cb8f48556272dee8545dcdf2d227f3c2dd02ea
AlexRaihn <iogan.dead@yandex.ru>
2025-03-12 08:52:04 +0300

    add konva

--- src/components/Private/TablePage/TablePageBody.vue
+++ src/components/Private/TablePage/TablePageBody.vue
@@ -1,10 +1,40 @@
-<script setup lang="ts">
-import TablePageCard from "./TablePageCard.vue";
-</script>
-
 <template>
   <div>
-    <div>тут будет стол</div>
-    <TablePageCard />
+    <v-stage
+      ref="stage"
+      :config="{
+        width,
+        height,
+        draggable: true,
+      }"
+    >
+      <v-layer>
+        <v-group ref="group">
+          <v-rect
+            v-for="n in 100"
+            :config="{
+              x: 20 + n * 5,
+              y: 50 + n * 5,
+              width: 100,
+              height: 100,
+              fill: 'white',
+              shadowBlur: 10,
+            }"
+          />
+        </v-group>
+      </v-layer>
+    </v-stage>
   </div>
 </template>
+
+<script setup lang="ts">
+const width = window.innerWidth;
+const height = window.innerHeight;
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import naive from "naive-ui";
+import VueKonva from "vue-konva";
 
 const app = createApp(App);
 
-app.use(router).use(naive);
+app.use(router).use(naive).use(VueKonva);
 
 app.mount("#app");
